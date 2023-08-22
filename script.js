b7e9f046cb2009c5c0f8b5298bb2631e7e7bd0e1
MeriJokhadze <jokhadzemeri1@gmail.com>
2023-08-22 23:14:12 +0400

    Update script.js
    updated

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ console.log(wordsNew);
 
 
 
-// მოცემულია სტრინგი - ‘12345’, გაიგეთ ამ რიცხვებსის ჯამი.
+// 5. მოცემულია სტრინგი - ‘12345’, გაიგეთ ამ რიცხვებსის ჯამი.
 // ( უნდა გადაიყავნოთ მასივში, გათვალოთ ჯამი, და შემდგ ეს სტრინგი გადაიყავნოთ რიცხვში)
 
 let numberString = "12345";
@@ -63,7 +63,7 @@ console.log(sumLast);
 
 
 
-// მოცემულია მასივი, შემდეგი მნიშვნელობებით -let array = ['html', 100, 'css', 'javascript', 25, 10, 'python', 11];
+// 6. მოცემულია მასივი, შემდეგი მნიშვნელობებით -let array = ['html', 100, 'css', 'javascript', 25, 10, 'python', 11];
 // გამოიტანეთ მარტო ციფრები;
 let array = ['html', 100, 'css', 'javascript', 25, 10, 'python', 11];
 
@@ -71,7 +71,7 @@ let newResult = array.filter(Number);
 console.log(newResult);
 
 
-// მოცემულია მასიცი:
+// 7. მოცემულია მასიცი:
 
 
 // შექმენით ახალი მასივი map ფუნქციის საშუალებით შემდეგი ლოგიკით: 
@@ -91,14 +91,21 @@ console.log(newArray2);
 
 
 
-// მოცემულია სტრინგი - ‘javascript’, მიიღეთ შემოტრიალებული ვარიანტი მეთოდების გარეშე ( for loop ით);
+// 8. მოცემულია სტრინგი - ‘javascript’, მიიღეთ შემოტრიალებული ვარიანტი მეთოდების გარეშე ( for loop ით);
 
-// let stringJ = 'javascript';
+let stringJ = 'javascript';
+let word = 'javascript';
 
-// item = 0 ; 
+for (let i = word.length; i < 0; i++) {
+    console.log(i);
+}  // amis logika ar mesmis 
 
 
-// მოცემულია სტრინგი: let info= ‘Good day”, ამოიღეთ სიტყვა day და ჩასვით ახალ ცვლადში slice მეთოდის საშუელებით.
+
+
+
+
+// 9. მოცემულია სტრინგი: let info= ‘Good day”, ამოიღეთ სიტყვა day და ჩასვით ახალ ცვლადში slice მეთოდის საშუელებით.
 
 let info = "good day";
 
@@ -106,6 +113,65 @@ let newInfo = info.slice("5","8");
 console.log(newInfo);
 
 
-// დაწერეთ ფუნქცია, რომელიც პარამეტრსდ იღებს სტრინგს, და დააბრუნებს ჩაწერილი სტრინგის სიგრძეს.
+// 10. daweრეთ ფუნქცია, რომელიც პარამეტრსდ იღებს სტრინგს, და დააბრუნებს ჩაწერილი სტრინგის სიგრძეს.
+
+let logString = string => string.length;
+
+console.log(logString("mery"));
+
+
+// 11.  Prompt ფუნქციის საშუალებით კითხეთ მომხმარებელს შემდეგი კითხვა: 
+//საქართველოს დედაქალაქი? სწორი პასუხი ჩაწერეთ პატარა ასობიეთ და  შეინახეთ ცვლადში.
+// როდესაც მიიღებთ პასუხს მომხმარებლისგან, გადაიყვანეთ ეს პასუხი პატარა ასოებად და შეადარეთ სწორ პასუხს. 
+//თუ მომხმარებლის პასუხი სწორია, დაბეჭდეთ სწორია, თუ არა მაშინ დაბეჭდეთ არასწორია.
+
+
+//  ნოუდ ჯიეზით ვერ ვამოწმებ ფრომფთ ფუნქციას მგონი 
+
+// let question = prompt("საქართველოს დედაქალაქი? ");
+// let correct = "tbilisi";
+
+// let resultt = function (question, correct){
+//     if (question.toLowerCase() === "tbilisi"){
+//         return correct;
+//     }else{
+//         return false;
+//     }
+// }
+
+// console.log(resultt(question,correct));
+
+
+// 12. მოცემულია: 
+//დაწერეთ ფუნქცია, რომელიც პარამეტრად იღებს ამ ცვლადს, და შეამოწმეთ indexof მეთოდის საშუალებით არის თუ არა
+// ამ ცვლადში “https://“. თუ კი გამოიტანეთ - კი მოიცავს, თუ არა - არა, არ მოიცავს.
+
+let linko = "https://google.com";
+
+let linkChecker = function(linko){
+    if (linko.indexOf("https://") !== -1) {
+        return console.log("კი მოიცავს");
+}else {
+    return console.log("არ მოიცავს ");
+}
+}
+
+linkChecker(linko);
+
+
+
+
+
+// 13. მოცემულია მასივი, რომელშიც არის ერთზე მეტი მნიშვნელობა, მაგრამ კონკრეტულად რამდენი არ ვიცით, 
+//შეიძლება 100 ან 200 ან 10. მაგ: let fruits = [“apple”, “mango”, “avocado”,”kiwi”]  splice მეთოდის საშუალებით 
+//წაშალეთ ბოლოს წინა მნიშვნელობა და მავის მავივრად ჩასვით “strawberry”. 
+
+let fruits = ["ვაშლი", "მსხალი", "ატამი", "ქლიავი"]; 
+
+fruits.splice(fruits.length-1, 1, "კივი");
+
+console.log(fruits);
+
+
 
 
